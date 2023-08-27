92f931c21fedc255d7efafd733a8291a2221403c
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2023-08-27 11:09:07 +0200

    Remove unused package

--- autopatchnethersx2.sh
+++ autopatchnethersx2.sh
@@ -3,7 +3,7 @@
 pkg upgrade -y
 termux-setup-storage
 ln -s /storage/emulated/0/NetherSX2 /data/data/com.termux/files/home/storage/NetherSX2
-pkg install aapt2 apksigner git wget -y
+pkg install aapt2 apksigner git -y
 git clone https://github.com/Trixarian/NetherSX2-patch.git
 cd NetherSX2-patch || exit
 cp ~/storage/NetherSX2/15210-v1.5-4248-noads.apk 15210-v1.5-4248-noads.apk
