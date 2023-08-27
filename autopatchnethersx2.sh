52afb250b0787ec6dfd7a4556badca17e2f83833
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2023-08-27 11:18:43 +0200

    Update autopatchnethersx2.sh

--- autopatchnethersx2.sh
+++ autopatchnethersx2.sh
@@ -1,9 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/bash
 # Installing aapt and apksigner
 pkg upgrade -y
+pkg install aapt2 apksigner git -y
 termux-setup-storage
 ln -s /storage/emulated/0/NetherSX2 /data/data/com.termux/files/home/storage/NetherSX2
-pkg install aapt2 apksigner git -y
 git clone https://github.com/Trixarian/NetherSX2-patch.git
 cd NetherSX2-patch || exit
 cp ~/storage/NetherSX2/15210-v1.5-4248-noads.apk 15210-v1.5-4248-noads.apk
