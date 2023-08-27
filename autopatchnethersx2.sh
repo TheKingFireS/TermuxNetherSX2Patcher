febf8c21e598b2616fbe2678b1b536f36ccc1404
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2023-08-27 13:10:29 +0200

    Update autopatchnethersx2.sh

--- autopatchnethersx2.sh
+++ autopatchnethersx2.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
-# Installing aapt and apksigner
+# Installing aapt, git and cloning repo.
 pkg upgrade -y
-pkg install git -y
+pkg install aapt git -y # it will uninstall some package due to crappy packages maintainer practice.
 git clone https://github.com/Trixarian/NetherSX2-patch.git
 cd NetherSX2-patch || exit
 cp /storage/emulated/0/NetherSX2/15210-v1.5-4248-noads.apk 15210-v1.5-4248-noads.apk
