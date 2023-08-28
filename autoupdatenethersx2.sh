36fd9c8b1ca6c354deace2a64abe60bdd9684d47
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2023-08-28 09:21:31 +0200

    Update autoupdatenethersx2.sh

--- autoupdatenethersx2.sh
+++ autoupdatenethersx2.sh
@@ -5,3 +5,4 @@ git pull https://github.com/Trixarian/NetherSX2-patch.git -b main
 ./remove-adservices.sh
 ./update-files.sh
 cp 15210-v1.5-4248-noads.apk /storage/emulated/0/NetherSX2/15210-v1.5-4248-noads-patched.apk
+echo "Done! Output APK should be in /storage/emulated/0/NetherSX2/. Check it in File manager."
