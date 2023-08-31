8ef5e5be1aacdb245b4055701bf06b163ad4513c
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2023-08-31 14:24:30 +0200

    Oops, mistake

--- autoupdatenethersx2.sh
+++ autoupdatenethersx2.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 # Update patch
 cd NetherSX2-patch || exit
-git pull https://github.com/Trixarian/NetherSX2-patch.git -b main
+git pull https://github.com/Trixarian/NetherSX2-patch.git main
 ./remove-adservices.sh
 ./update-files.sh
 cp 15210-v1.5-4248-noads.apk /storage/emulated/0/NetherSX2/15210-v1.5-4248-noads-patched.apk
