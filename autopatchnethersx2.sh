ec8329c163a6263d31de2b4b80d8063a2987905e
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2023-09-03 15:06:32 +0200

    Update autopatchnethersx2.sh

--- autopatchnethersx2.sh
+++ autopatchnethersx2.sh
@@ -5,8 +5,7 @@ pkg install aapt git -y # it will uninstall some package due to crappy packages 
 git clone https://github.com/Trixarian/NetherSX2-patch.git
 cd NetherSX2-patch || exit
 cp /storage/emulated/0/NetherSX2/15210-v1.5-4248-noads.apk 15210-v1.5-4248-noads.apk
-chmod +x remove-adservices.sh update-files.sh
-./remove-adservices.sh
-./update-files.sh
+chmod +x patch-apk.sh
+./patch-apk.sh
 cp 15210-v1.5-4248-noads.apk /storage/emulated/0/NetherSX2/15210-v1.5-4248-noads-patched.apk
 echo "Done! Output APK should be in /storage/emulated/0/NetherSX2/. Check it in File manager."
