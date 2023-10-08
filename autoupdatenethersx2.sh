b7575f00de219cd031472243e43e1f88b73b556c
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2023-10-08 20:28:39 +0200

    Update autoupdatenethersx2.sh

--- autoupdatenethersx2.sh
+++ autoupdatenethersx2.sh
@@ -1,10 +1,13 @@
 #!/data/data/com.termux/files/usr/bin/bash
 # Update patch
-cd NetherSX2-patch || exit
-rm patch-apk.sh update-files.sh
-git pull https://github.com/Trixarian/NetherSX2-patch.git main
-curl -LO "https://raw.githubusercontent.com/Trixarian/NetherSX2-patch/main/patch-apk.sh"
-curl -LO "https://raw.githubusercontent.com/Trixarian/NetherSX2-patch/main/update-files.sh"
+if [ "$(pwd)" == "/data/data/com.termux/files/home/NetherSX2-patch" ]; then
+	mv autoupdatenethersx2.sh ../autoupdatenethersx2.sh
+	printf "\e[1;91mPlease run "cd .." before try again run "./autoupdatenethersx2.sh"!\e[0m\n"
+	exit 1
+fi
+wget "https://github.com/Trixarian/NetherSX2-patch/releases/download/1.6/NetherSX2-patch.zip"
+unzip NetherSX2-patch.zip
+rm NetherSX2-patch.zip
 chmod +x update-files.sh patch-apk.sh
 ./update-files.sh
 cp 15210-v1.5-4248-noads.apk /storage/emulated/0/NetherSX2/15210-v1.5-4248-noads-patched.apk
